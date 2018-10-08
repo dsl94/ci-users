dc5e72431dd2026b9d0d9eb9b50a461b41257696
Nemanja <nemanjabosnjane@gmail.com>
2018-10-08 21:59:37 +0200

    initi

---
+++ src/main/java/com/ciusers/CiUsersApplication.java
@@ -0,0 +1,12 @@
+package com.ciusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiUsersApplication.class, args);
+	}
+}
