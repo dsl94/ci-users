495eeedb9efab9224c7ca4615abefbe693ae1fe0
nemanja.petrovic <nemanja.petrovic@mapp.com>
2018-10-09 09:41:19 +0200

    Eureka added

--- src/main/java/com/ciusers/CiUsersApplication.java
+++ src/main/java/com/ciusers/CiUsersApplication.java
@@ -2,8 +2,10 @@ package com.ciusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CiUsersApplication {
 
 	public static void main(String[] args) {
